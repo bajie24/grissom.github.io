ac93da382b093fb1162e6aac1c3188738378abd1
bajie24 <1095122820@qq.com>
2017-03-14 22:07:06 +0800

    Update index.js

--- front-end/index.js
+++ front-end/index.js
@@ -1,1 +1,11 @@
-#shoahoudailai 
+var header = document.getElementById('header');
+var content = document.querySelector('.content');
+window.onscroll = function () {
+    var scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
+    if(scrolltop > 20){
+        header.style.opacity = 0.8;
+    }else {
+        header.style.opacity = 1;
+        header.style.transition = 'opacity 0.3s ease';
+    }
+}
